09aa7275da8ddb27722bbb08d67cd67c02e16694
Ahmed <sanada927@gmail.com>
2025-12-25 17:21:47 +0200

    Add UI for updating a new school class and implement update school class business logic

--- SchoolManagementSystem/Controllers/SchoolClassController.cs
+++ SchoolManagementSystem/Controllers/SchoolClassController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using School.Data.Entities;
-using School.Service.ServiceImplementations;
 using School.Service.ServiceInterfaces;
 using SchoolManagementSystem.Models;
 
@@ -10,13 +9,14 @@ namespace SchoolManagementSystem.Controllers
     public class SchoolClassController(ISchoolClassService schoolClassService) : Controller
     {
         [HttpGet]
-        public async Task<IActionResult> GetSchoolClass()
+        public async Task<IActionResult> GetSchoolClass(int? editingId = null)
         {
             var classes = await schoolClassService.GetAllAsync();
             var model = new SchoolClassViewModel()
             {
                 Classes = classes,
-                Class = new School_Class()
+                EditingId = editingId,
+                Class = editingId.HasValue ? await schoolClassService.GetByIdAsync(editingId.Value) : new School_Class() // either empty school class or the one to edit
             };
             return View(model);
         }
@@ -43,6 +43,31 @@ namespace SchoolManagementSystem.Controllers
             }
             viewModel.Classes = await schoolClassService.GetAllAsync();
             viewModel.EditingId = null;
+            return View("GetSchoolClass", viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(SchoolClassViewModel viewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await schoolClassService.Update(viewModel.Class);
+                    TempData["Success"] = "Class updated successfully.";
+                    return RedirectToAction("GetSchoolClass");
+                }
+            }
+            catch (DuplicateNameException ex)
+            {
+                ModelState.AddModelError("Class.Name", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", $"An error occurred: {ex.Message}");
+                Console.WriteLine(ex);
+            }
+            viewModel.Classes = await schoolClassService.GetAllAsync();
+            viewModel.EditingId = viewModel.Class.Id;
             return View("GetSchoolClass", viewModel);
         }
     }
